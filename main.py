456f87fb035ef7f6ded9b3b800d668e64ac23538
Leonardo Ikeda <80907803+ikedss@users.noreply.github.com>
2022-11-18 22:18:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 import Grafo as gf
 import random
 
@@ -45,70 +47,63 @@ class Grafo_Random:
 
                 else:
                     if not self.grafo.tem_aresta(nome_aleatorio_1, nome_aleatorio_2):
-                        self.grafo.adiciona_aresta(nome_aleatorio_1,
-                                                   nome_aleatorio_2,
-                                                   n_rand)
-
-                        self.grafo.adiciona_aresta(nome_aleatorio_2,
-                                                   nome_aleatorio_1,
-                                                   n_rand)
+                        self.grafo.adiciona_aresta_n_direcionado(nome_aleatorio_1,
+                                                                 nome_aleatorio_2,
+                                                                 n_rand)
 
         else:
             print("Direcionado é bool")
 
-    def transposto(self, vertice1, vertice2, grafo):
-        if grafo.tem_aresta(vertice1, vertice2):
-            peso_ = grafo.peso(vertice1, vertice2)
-            grafo.remove_aresta(vertice1, vertice2)
-            grafo.adiciona_aresta(vertice2, vertice1, peso_)
-        else:
-            return False
-
-    def transpor_grafo(self):
-        for chave, valores in self.grafo.grafo.items():
-            if len(valores) > 0:
-                for valor in valores:
-                    peso = self.grafo.peso(chave, valor[0])
-                    self.grafo.remove_aresta(chave, valor[0])
-                    self.grafo.adiciona_aresta(valor[0], chave, peso)
-        print(self.grafo.imprime_lista_adjacencias())
-
-    def dag(self, grafo):
-        if grafo.direcionado:
-            print("oi")
-        else:
-            print("Parametro invalido")
 
     def Kahn_Algorithm(self):
-        lista = []
-        total = list(self.grafo.grafo.keys())
-        grau_zerado = [0] * (self.grafo.numero_vertices())
-        for vertice in total:
-            grau_entrada = self.grafo.grau_entrada(vertice)
-            numero_de_vertices = grau_entrada
-            if numero_de_vertices == 0:
-                lista.append(vertice)
-                self.grafo.remove_vertice(vertice)
+        if self.direcionado:
+            lista = []
+            total = list(self.grafo.grafo.keys())
+            grau_zerado = [0] * (self.grafo.numero_vertices())
+            for vertice in total:
+                grau_entrada = self.grafo.grau_entrada(vertice)
+                numero_de_vertices = grau_entrada
+                if numero_de_vertices == 0:
+                    lista.append(vertice)
+                    self.grafo.remove_vertice(vertice)
 
-        sorted_list = []
-        while lista:
-            vertice_lista = lista.pop()
-            sorted_list.append(vertice_lista)
-            for vertice_vizinho in self.grafo.grafo.get(vertice_lista, []):
-                grau_zerado[vertice_vizinho] -= 1
-                if grau_zerado[vertice_vizinho] == 0:
-                    lista.append(vertice_vizinho)
+            sorted_list = []
+            while lista:
+                vertice_lista = lista.pop()
+                sorted_list.append(vertice_lista)
+                for vertice_vizinho in self.grafo.grafo.get(vertice_lista, []):
+                    grau_zerado[vertice_vizinho] -= 1
+                    if grau_zerado[vertice_vizinho] == 0:
+                        lista.append(vertice_vizinho)
 
-        if len(sorted_list) != len(grau_zerado):
-            return "Grafo tem ciclos"
+            if len(sorted_list) != len(grau_zerado):
+                return True
+            else:
+                return print(sorted_list)
         else:
-            return sorted_list
+            return print("grafo não é direcionado")
+
+    def dag(self):
+        while self.Kahn_Algorithm():
+            self.grafo.remove_aresta_vertice(random.choice(self.grafo.maior_grau_sem_n()))
+
+    def transformador_conexo(self):
+        if self.direcionado:
+            return print("grafo é direcionado")
+        else:
+            for vertice in self.grafo.grafo:
+                if self.grafo.grau(vertice) == 0:
+                    self.grafo.adiciona_aresta_n_direcionado(random.choice(self.grafo.nome_vertice()),
+                                               random.choice(self.grafo.nome_vertice()),
+                                               random.randint(0, 100))
+
+
 
 
 def main():
-    grafo_ramdom = Grafo_Random(50, 50, True)
-    grafo_ramdom.dag(grafo_ramdom)
-    print(grafo_ramdom.Kahn_Algorithm())
+    grafo_random = Grafo_Random(100, 150, True)
+    grafo_random_copy = copy.deepcopy(grafo_random)
+    grafo_random_copy.dag()
 
 
 if __name__ == '__main__':
